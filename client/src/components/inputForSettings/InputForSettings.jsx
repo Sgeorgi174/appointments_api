6a3c86f2efd3a351b3bf37813f0dcfef8854380e
Georgi <sgeorgi174@gmail.com>
2024-04-04 03:18:56 +0500

    developing bot-setting page

--- client/src/components/inputForSettings/InputForSettings.jsx
+++ client/src/components/inputForSettings/InputForSettings.jsx
@@ -1,19 +1,49 @@
 import { useState } from "react";
 import styles from "./InputForSettings.module.css";
 
-export const InputForSettings = ({ placeHolder, setState }) => {
+export const InputForSettings = ({ placeHolder, setState, type }) => {
   const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
+
   const onChange = (event) => {
     setValue(event.target.value);
     setState(event.target.value);
+    setError(false); // Сброс ошибки при изменении значения
   };
-  return (
-    <input
-      value={value}
-      onChange={onChange}
-      className={styles.input}
-      placeholder={placeHolder}
-      type="text"
-    ></input>
+
+  const onBlur = () => {
+    if (!value.trim()) {
+      setError(true); // Установка ошибки, если значение пустое или состоит только из пробелов
+    }
+  };
+
+  return type ? (
+    <div className={styles.inputWrapper}>
+      <input
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={`${styles.input} ${error ? styles.error : ""}`}
+        placeholder={placeHolder}
+        type="text"
+      />
+      {error && (
+        <span className={styles.errorText}>Заполните обязательные поля</span>
+      )}
+    </div>
+  ) : (
+    <div className={styles.inputWrapper}>
+      <textarea
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={`${styles.input} ${error ? styles.error : ""}`}
+        placeholder={placeHolder}
+        type="text"
+      />
+      {error && (
+        <span className={styles.errorText}>Заполните обязательные поля</span>
+      )}
+    </div>
   );
 };
--- client/src/pages/botSetting/BotSetting.jsx
+++ client/src/pages/botSetting/BotSetting.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { GradientButton } from "../../components/gradientButton/GradientButton";
 import { Header } from "../../components/header/Header";
 import { InputForSettings } from "../../components/inputForSettings/InputForSettings";
@@ -5,25 +6,74 @@ import { Wrapper } from "../../components/wrapper/Wrapper";
 import styles from "./BotSetting.module.css";
 
 export const BotSetting = () => {
+  const [botSetting, setBotSetting] = useState({
+    botToken: "",
+    botName: "",
+    address: "",
+  });
+  const [botToken, setBotToken] = useState("");
+  const [botName, setBotName] = useState("");
+  const [address, setAddress] = useState("");
+  const [isSaved, setIsSaved] = useState(false);
+
+  const onClick = () => {
+    if (isSaved) {
+      setIsSaved(!isSaved);
+      console.log(isSaved);
+      console.log(botSetting);
+    } else {
+      setIsSaved(!isSaved);
+      console.log(isSaved);
+    }
+  };
+
+  useEffect(() => {
+    setBotSetting({ botName, address, botToken });
+  }, [botToken, botName, address]);
+
   return (
     <Wrapper wrapperClass={"wrapperForMobile"}>
       <Header firstLetter={"G"} />
       <h1 className={styles.title}>Настройка бота</h1>
-      <div className={styles.setiingsBox}>
-        <div className={styles.setiingBox}>
-          <h2 className={styles.inputTitle}>1. Скопируйте ваш token</h2>
-          <InputForSettings placeHolder={"token"} />
+      {isSaved ? (
+        <div className={styles.setiingsBox}>
+          <div className={styles.settingBox}>
+            <p className={styles.settingText}>Имя : {botName}</p>
+          </div>
+          <div className={styles.settingBox}>
+            <p className={styles.settingText}>Адрес : {address}</p>
+          </div>
         </div>
-        <div className={styles.setiingBox}>
-          <h2 className={styles.inputTitle}>2. Укажите имя для бота</h2>
-          <InputForSettings placeHolder={"имя бота"} />
+      ) : (
+        <div className={styles.setiingsBox}>
+          <div className={styles.inputBox}>
+            <h2 className={styles.inputTitle}>1. Скопируйте ваш token</h2>
+            <InputForSettings
+              setState={setBotToken}
+              type="input"
+              placeHolder={"token"}
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <h2 className={styles.inputTitle}>2. Укажите имя для бота</h2>
+            <InputForSettings
+              setState={setBotName}
+              type="input"
+              placeHolder={"имя бота"}
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <h2 className={styles.inputTitle}>3. Укажите адрес</h2>
+            <InputForSettings setState={setAddress} placeHolder={"ваш адрес"} />
+          </div>
         </div>
-        <div className={styles.setiingBox}>
-          <h2 className={styles.inputTitle}>3. Укажите адрес</h2>
-          <InputForSettings placeHolder={"ваш адрес"} />
-        </div>
-      </div>
-      <GradientButton buttonName="Сохранить" />
+      )}
+      <GradientButton
+        onClick={() => {
+          onClick();
+        }}
+        buttonName={isSaved ? "Редактировать" : "Сохранить"}
+      />
     </Wrapper>
   );
 };
