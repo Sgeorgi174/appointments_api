59b88e68e90fda986a7d1c6ad8e8b26b82acc4b1
Georgi <sgeorgi174@gmail.com>
2024-04-11 02:32:42 +0500

    changes

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -2,7 +2,7 @@ const formatDate = (date) => {
   const jsDate = new Date(date);
   console.log(jsDate, "jsData");
   const isoDate = new Date(
-    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+    Date.UTC(jsDate.getFullYear(), jsDate.getMonth(), jsDate.getDate())
   ).toISOString();
 
   return isoDate;
